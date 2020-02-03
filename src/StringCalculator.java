72301c99672b5a2b879ddb450bc00f91743b8f36
Lec337 <lec337@mail.usask.ca>
2020-02-03 17:49:14 -0600

    Added declarations and stubs for inputToInt() and Add()

--- src/StringCalculator.java
+++ src/StringCalculator.java
@@ -1,5 +1,24 @@
 // Leon Cadotte, 7Shifts Exercise
 public class StringCalculator {
 
+    /**
+     * converts the input string numbers into an array of integers
+     * @param input: String of the form "x,y,z..." where x,y,z are numbers, input cannot be the empty string
+     * @return integer array containing the numbers from the input string
+     * @throws NumberFormatException if input is an empty string
+     */
+    public int[] inputToInt(String input) throws NumberFormatException{
+        return new int[] {0};
+    }
+
+    /**
+     * Adds up the numbers in the string 'numbers', returns the integer resulting
+     * @param numbers: String of the form "x,y,z..." where x,y,z are numbers, if the empty string is input, 0 is returned
+     * @return integer, result of adding the integers in the string 'numbers', or 0 if numbers is the empty string
+     */
+    public int Add(String numbers){
+        return 0;
+    }
+
 
 }
