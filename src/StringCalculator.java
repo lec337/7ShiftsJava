e6605fc6b2de45ef5dbd46dc4a717642cbf472dd
lec337 <lec337@mail.usask.ca>
2020-02-04 08:54:58 -0600

    Added support for custom delimiters in inputToInt()

--- src/StringCalculator.java
+++ src/StringCalculator.java
@@ -1,26 +1,46 @@
+import java.util.Arrays;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 // Leon Cadotte, 7Shifts Exercise
 public class StringCalculator {
 
     /**
      * converts the input string numbers into an array of integers
-     * @param input: String of the form "x,y,z..." where x,y,z are numbers, input cannot be the empty string, input can contain
-     *             newlines
+     * @param input: String of the form "//<delimiter>\nx<delimiter>y<delimiter>z..." where x,y,z are numbers and <delimiter> is
+     *             a custom, special sequence of characters used to separate numbers, input cannot be the empty string, input can contain
+     *             newlines, delimiter cannot be the newline or a number
      * @return integer array containing the numbers from the input string
-     * @throws NumberFormatException if input is an empty string, or a string number cannot be converted to an integer
+     * @throws NumberFormatException if input is an empty string, or a string number cannot be converted to an integer,
+     * @throws IllegalArgumentException if input isn't lead by valid delimiter
      */
-    protected static int[] inputToInt(String input) throws NumberFormatException{
+    protected static int[] inputToInt(String input) throws NumberFormatException, IllegalArgumentException{
         if(input.isEmpty()){
             throw new NumberFormatException("Cannot convert an empty string to integers");
         }
-        // remove newlines
-        input = input.replaceAll("\n","");
+        // extract the custom delimiter using a regular expression
+        Matcher m = Pattern.compile("(^//.+\n)").matcher(input);
+        if(! m.find()){
+            // no matches for the general delimiter pattern were found in the input
+            throw new IllegalArgumentException("Input: " + input + " has invalid delimiter format");
+        }
+        // get the delimiter
+        String delimiter = m.group(0);
+        // remove the leading delimiter from input
+        input = input.substring(delimiter.length());
+        // delimiter contains leading '//' and ending '\n', remove these
+        delimiter = delimiter.replaceAll("^//","");
+        delimiter = delimiter.replaceAll("\n$","");
 
         // convert each number from 'input' to an integer
-        String[] stringNumbers = input.split(",");
+        String[] stringNumbers = input.split(delimiter);
+        System.out.println(Arrays.toString(stringNumbers));
         int[] integerNumbers = new int[stringNumbers.length];
         for(int i=0;i<stringNumbers.length;i++){
             try {
-                integerNumbers[i] = Integer.parseInt(stringNumbers[i]);
+                // remove newlines from individual strings if any exist
+                String currentStr = stringNumbers[i].replaceAll("\n","");
+                integerNumbers[i] = Integer.parseInt(currentStr);
             }catch(NumberFormatException e){
                 throw new NumberFormatException("Cannot convert string number: " + stringNumbers[i]
                 + " to an integer");
